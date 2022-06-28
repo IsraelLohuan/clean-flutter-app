1c7c088aa57a60e539a3ba88f764a7adb8b4e3dd
IsraelLohuan <srlohuan@gmail.com>
2022-06-27 21:19:09 -0300

    ensure RemoteAuthentication throws UnexpectedError on 200 with invalid data

--- lib/data/models/remote_account_model.dart
+++ lib/data/models/remote_account_model.dart
@@ -1,3 +1,4 @@
+import 'package:ForDev/data/http/http.dart';
 import 'package:ForDev/domain/entities/account_entity.dart';
 
 class RemoteAccountModel {
@@ -5,8 +6,14 @@ class RemoteAccountModel {
 
   RemoteAccountModel(this.accessToken);
 
-  factory RemoteAccountModel.fromJson(Map json) =>
-    RemoteAccountModel(json['accessToken']);
+  factory RemoteAccountModel.fromJson(Map json) {
+    if(!json.containsKey('accessToken')) {
+      throw HttpError.invalidData;
+    }
+
+    return RemoteAccountModel(json['accessToken']);
+  }
+    
 
   AccountEntity toEntity() => AccountEntity(accessToken);
 }
