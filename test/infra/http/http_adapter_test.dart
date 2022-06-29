293126c8ae962216197ca00d6e3f6d886ff6f51c
IsraelLohuan <srlohuan@gmail.com>
2022-06-29 20:04:55 -0300

    ensure HttpAdapter throws BadRequestError on 400

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,4 +1,5 @@
 
+import 'package:ForDev/data/http/http.dart';
 import 'package:ForDev/infra/http/http.dart';
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -82,6 +83,22 @@ void main() {
       final response = await sut.request(url: url, method: 'post');
 
       expect(response, null);
+    });
+
+    test('Should return BadRequestError if post returns 400', () async {
+      mockResponse(400, body: '');
+      
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.badRequest));
+    });
+
+    test('Should return BadRequestError if post returns 400', () async {
+      mockResponse(400);
+      
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.badRequest));
     });
   });
 }
