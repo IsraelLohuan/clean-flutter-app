b095cfc5d6bdb6698ac28b568e5b6ecdbcb358fb
IsraelLohuan <srlohuan@gmail.com>
2022-06-29 20:12:40 -0300

    ensure HttpAdapter throws ForbiddenError  on 403

--- lib/infra/http/http_adapter.dart
+++ lib/infra/http/http_adapter.dart
@@ -27,10 +27,12 @@ class HttpAdapter implements HttpClient {
       return response.body.isEmpty ? null : jsonDecode(response.body);
     } else if(response.statusCode == 204) {
       return null;
-    } else if (response.statusCode == 400) {
+    } else if(response.statusCode == 400) {
       throw HttpError.badRequest;
-    } else if (response.statusCode == 401) {
+    } else if(response.statusCode == 401) {
       throw HttpError.unauthorized;
+    } else if(response.statusCode == 403){
+      throw HttpError.forbidden;
     } else {
       throw HttpError.serverError;
     }
