2e2693c2dfb1a84fa644a3afef92668473e59fcd
IsraelLohuan <srlohuan@gmail.com>
2022-06-29 19:29:26 -0300

    ensure HttpAdapter calls post with correct Body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
@@ -20,7 +22,7 @@ class HttpAdapter {
       'accept': 'application/json'
     };
 
-    await client.post(url, headers: headers);
+    await client.post(url, headers: headers, body: jsonEncode(body));
   }
 }
 
@@ -40,14 +42,15 @@ void main() {
 
   group('post', () {
     test('Should call post with correct values', () async {
-      await sut.request(url: url, method: 'post');
+      await sut.request(url: url, method: 'post', body: {'any_key': 'any_value'});
 
       verify(client.post(
         url, 
         headers: {
           'content-type': 'application/json',
           'accept': 'application/json'
-        }
+        },
+        body: '{"any_key":"any_value"}'
       ));
     });
   });
