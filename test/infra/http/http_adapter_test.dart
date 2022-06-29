b848a3a3dfa180ad1c5c13d03ed894b1185465e8
IsraelLohuan <srlohuan@gmail.com>
2022-06-29 19:50:50 -0300

    move duplicated code to the setup method

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -42,10 +42,19 @@ void main() {
   });
 
   group('post', () {
-    test('Should call post with correct values', () async {
-      when(client.post(any, headers: anyNamed('headers'), body: anyNamed('body')))
-        .thenAnswer((_) async => Response('{"any_key":"any_value"}', 200));
 
+    PostExpectation mockRequest() => 
+      when(client.post(any, headers: anyNamed('headers'), body: anyNamed('body')));
+
+    void mockResponse(int statusCode, {String body = '{"any_key":"any_value"}'}) {
+      mockRequest().thenAnswer((_) async => Response(body, statusCode));
+    }
+
+    setUp(() {
+      mockResponse(200);
+    });
+
+    test('Should call post with correct values', () async {
       await sut.request(url: url, method: 'post', body: {'any_key': 'any_value'});
 
       verify(client.post(
@@ -59,9 +68,6 @@ void main() {
     });
 
     test('Should call post without body', () async {
-      when(client.post(any, headers: anyNamed('headers'), body: anyNamed('body')))
-        .thenAnswer((_) async => Response('{"any_key":"any_value"}', 200));
-
       await sut.request(url: url, method: 'post');
 
       verify(client.post(
@@ -71,18 +77,14 @@ void main() {
     });
 
     test('Should return data if post returns 200', () async {
-      when(client.post(any, headers: anyNamed('headers')))
-        .thenAnswer((_) async => Response('{"any_key":"any_value"}', 200));
-
       final response = await sut.request(url: url, method: 'post');
 
       expect(response, {'any_key': 'any_value'});
     });
 
     test('Should return null if post returns 200 with no data', () async {
-      when(client.post(any, headers: anyNamed('headers')))
-        .thenAnswer((_) async => Response('', 200));
-
+      mockResponse(200, body: '');
+      
       final response = await sut.request(url: url, method: 'post');
 
       expect(response, null);
