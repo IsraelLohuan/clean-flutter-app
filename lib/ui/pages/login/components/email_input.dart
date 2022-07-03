6fa80bd5c1945559838c51662b31ba7db73add7a
IsraelLohuan <srlohuan@gmail.com>
2022-07-03 16:03:31 -0300

    move EmailInput to its own file

---
+++ lib/ui/pages/login/components/email_input.dart
@@ -0,0 +1,24 @@
+import 'package:ForDev/ui/pages/login/login_presenter.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class EmailInput extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final presenter = Provider.of<LoginPresenter>(context);
+    return StreamBuilder<String>(
+      stream: presenter.emailErrorStream,
+      builder: (context, snapshot) {
+        return TextFormField(
+          decoration: InputDecoration(
+            labelText: 'Email',
+            icon: Icon(Icons.email, color: Theme.of(context).primaryColorLight,),
+            errorText: snapshot.data?.isEmpty == true ? null : snapshot.data 
+          ),
+          keyboardType: TextInputType.emailAddress,
+          onChanged: presenter.validateEmail,
+        );
+      }
+    );
+  }
+}
--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -1,6 +1,9 @@
 import 'package:ForDev/ui/components/components.dart';
 import 'package:ForDev/ui/pages/pages.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'components/email_input.dart';
 
 class LoginPage extends StatefulWidget {
   final LoginPresenter presenter;
@@ -40,55 +43,45 @@ class _LoginPageState extends State<LoginPage> {
                 HeadLine1(text: 'Login',),
                 Padding(
                   padding: const EdgeInsets.all(32),
-                  child: Form(
-                    child: Column(
-                      children: [
-                        StreamBuilder<String>(
-                          stream: widget.presenter.emailErrorStream,
-                          builder: (context, snapshot) {
-                            return TextFormField(
-                              decoration: InputDecoration(
-                                labelText: 'Email',
-                                icon: Icon(Icons.email, color: Theme.of(context).primaryColorLight,),
-                                errorText: snapshot.data?.isEmpty == true ? null : snapshot.data 
-                              ),
-                              keyboardType: TextInputType.emailAddress,
-                              onChanged: widget.presenter.validateEmail,
-                            );
-                          }
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(top: 8, bottom: 32),
-                          child: StreamBuilder<String>(
-                            stream: widget.presenter.passwordErrorStream,
+                  child: Provider(
+                    create: (_) => widget.presenter,
+                    child: Form(
+                      child: Column(
+                        children: [
+                          EmailInput(),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 8, bottom: 32),
+                            child: StreamBuilder<String>(
+                              stream: widget.presenter.passwordErrorStream,
+                              builder: (context, snapshot) {
+                                return TextFormField(
+                                  decoration: InputDecoration(
+                                    labelText: 'Senha',
+                                    icon: Icon(Icons.lock, color: Theme.of(context).primaryColorLight,),
+                                    errorText: snapshot.data?.isEmpty == true ? null : snapshot.data 
+                                  ),
+                                  keyboardType: TextInputType.emailAddress,
+                                  onChanged: widget.presenter.validatePassword,
+                                );
+                              }
+                            ),
+                          ),
+                          StreamBuilder<bool>(
+                            stream: widget.presenter.isFormValidStream,
                             builder: (context, snapshot) {
-                              return TextFormField(
-                                decoration: InputDecoration(
-                                  labelText: 'Senha',
-                                  icon: Icon(Icons.lock, color: Theme.of(context).primaryColorLight,),
-                                  errorText: snapshot.data?.isEmpty == true ? null : snapshot.data 
-                                ),
-                                keyboardType: TextInputType.emailAddress,
-                                onChanged: widget.presenter.validatePassword,
+                              return RaisedButton(
+                                onPressed: snapshot.data == true ? widget.presenter.auth : null,
+                                child: Text('Entrar'.toUpperCase()),
                               );
                             }
                           ),
-                        ),
-                        StreamBuilder<bool>(
-                          stream: widget.presenter.isFormValidStream,
-                          builder: (context, snapshot) {
-                            return RaisedButton(
-                              onPressed: snapshot.data == true ? widget.presenter.auth : null,
-                              child: Text('Entrar'.toUpperCase()),
-                            );
-                          }
-                        ),
-                        FlatButton.icon(
-                          onPressed: () {}, 
-                          icon: Icon(Icons.person), 
-                          label: Text('Criar Conta')
-                        )
-                      ],
+                          FlatButton.icon(
+                            onPressed: () {}, 
+                            icon: Icon(Icons.person), 
+                            label: Text('Criar Conta')
+                          )
+                        ],
+                      ),
                     ),
                   ),
                 )
@@ -100,5 +93,7 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 }
+
+
 
 
