4ff9fd24542b9141304a3ccb7d35b81e138e7cc9
IsraelLohuan <srlohuan@gmail.com>
2022-07-05 21:46:00 -0300

    ensure EmailValidation returns erro  if email is in valid

--- test/validation/validators/email_validation_test.dart
+++ test/validation/validators/email_validation_test.dart
@@ -8,7 +8,9 @@ class EmailValidation implements FieldValidation {
 
   @override
   String validate(String value) {
-    return null;
+    final regex = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    final isValid = value?.isNotEmpty != true || regex.hasMatch(value);
+    return isValid ? null : 'Campo inválido';
   }  
 }
 
@@ -30,5 +32,9 @@ void main() {
 
   test('Should return null if email is valid', () {
     expect(sut.validate('srlohuan@gmail.com'), null);
+  });
+
+  test('Should return error if email is invalid', () {
+    expect(sut.validate('srlohuangmail.com'), 'Campo inválido');
   });
 }
