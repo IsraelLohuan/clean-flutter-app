8cfbb93c8ad28cb67c6a4dba0181016dada3ed16
IsraelLohuan <srlohuan@gmail.com>
2022-07-05 21:59:29 -0300

    ensure ValidationComposite returns null if all validation returns null or empty

---
+++ test/validation/validators/validation_composite_test.dart
@@ -0,0 +1,36 @@
+import 'package:ForDev/presentation/protocol/protocols.dart';
+import 'package:ForDev/validation/protocols/field_validation.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:meta/meta.dart';
+import 'package:mockito/mockito.dart';
+
+class ValidationComposite implements Validation {
+  final List<FieldValidation> validations;
+
+  ValidationComposite(this.validations);
+
+  @override
+  String validate({@required String field, @required String value}) {
+    return null;
+  } 
+}
+
+class FieldValidationSpy extends Mock implements FieldValidation {}
+
+void main() {
+  test('Should return null if all validations returns null or empty', () {
+    final validation1 = FieldValidationSpy();
+    when(validation1.field).thenReturn('any_field');
+    when(validation1.validate(any)).thenReturn(null);
+
+    final validation2 = FieldValidationSpy();
+    when(validation1.field).thenReturn('any_field');
+    when(validation1.validate(any)).thenReturn('');
+
+    final sut = ValidationComposite([validation1, validation2]);
+
+    final error = sut.validate(field: 'any_field', value: 'any_value');
+
+    expect(error, null);
+  });
+}
