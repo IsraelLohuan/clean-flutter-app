83fde14d0aab6def0f93032f05e119c03163ad0e
IsraelLohuan <srlohuan@gmail.com>
2022-07-11 21:14:30 -0300

    ensure GetxLoginPresenter calls SaveCurrentAccount with correct values

--- lib/main/factories/pages/login/login_presenter_factory.dart
+++ lib/main/factories/pages/login/login_presenter_factory.dart
@@ -13,6 +13,7 @@ LoginPresenter makeStreamLoginPresenter() {
 LoginPresenter makeGetXLoginPresenter() {
   return GetxLoginPresenter(
     authentication: makeRemoteAuthentication(),
-    validation: makeLoginValidation()
+    validation: makeLoginValidation(),
+    saveCurrentAccount: makeLocalSaveCurrentAccount()
   );
 }
--- lib/presentation/presenters/getx_login_presenter.dart
+++ lib/presentation/presenters/getx_login_presenter.dart
@@ -9,6 +9,7 @@ import 'package:ForDev/presentation/protocol/protocols.dart';
 class GetxLoginPresenter extends GetxController implements LoginPresenter {
   final Validation validation;
   final Authentication authentication;
+  final SaveCurrentAccount saveCurrentAccount;
 
   String _email;
   String _password;
@@ -25,7 +26,11 @@ class GetxLoginPresenter extends GetxController implements LoginPresenter {
   Stream<bool> get isFormValidStream => _isFormValid.stream;
   Stream<bool> get isLoadingStream => _isLoading.stream;
 
-  GetxLoginPresenter({@required this.validation, @required this.authentication});
+  GetxLoginPresenter({
+    @required this.validation, 
+    @required this.authentication, 
+    @required this.saveCurrentAccount
+  });
 
   void validateEmail(String email) {
     _email = email;
@@ -49,7 +54,8 @@ class GetxLoginPresenter extends GetxController implements LoginPresenter {
   Future<void> auth() async {
     _isLoading.value = true;
     try {
-      await authentication.auth(AuthenticationParams(email: _email, secret: _password));
+      final account = await authentication.auth(AuthenticationParams(email: _email, secret: _password));
+      await saveCurrentAccount.save(account);
     } on DomainError catch(error) {
       _mainError.value = error.description;
     }
