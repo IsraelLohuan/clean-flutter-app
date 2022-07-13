e64d0771bdbfffa03a221e55ce75d8af84729ed8
IsraelLohuan <srlohuan@gmail.com>
2022-07-13 14:35:31 -0300

    commit backup

--- test/ui/pages/signup_page_test.dart
+++ test/ui/pages/signup_page_test.dart
@@ -1,10 +1,42 @@
 import 'dart:async';
+import 'package:ForDev/ui/helpers/helpers.dart';
+import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
 import 'package:ForDev/ui/pages/pages.dart';
+import 'package:mockito/mockito.dart';
+
+class SignUpPresenterSpy extends Mock implements SignUpPresenter {}
 
 void main() {
+  SignUpPresenter presenter;
+  StreamController<UiError> nameErrorController;
+  StreamController<UiError> emailErrorController;
+  StreamController<UiError> passwordErrorController;
+  StreamController<UiError> passwordConfirmationErrorController;
+  
+  void initStreams() {
+    nameErrorController = StreamController<UiError>();
+    emailErrorController = StreamController<UiError>();
+    passwordErrorController = StreamController<UiError>();
+    passwordConfirmationErrorController = StreamController<UiError>();
+  }
+
+  void mockStreams() {
+    when(presenter.nameErrorStream).thenAnswer((_) => nameErrorController.stream);
+    when(presenter.emailErrorStream).thenAnswer((_) => emailErrorController.stream);
+    when(presenter.passwordErrorStream).thenAnswer((_) => passwordErrorController.stream);
+    when(presenter.passwordConfirmationErrorStream).thenAnswer((_) => passwordConfirmationErrorController.stream);
+  }
+
+  void closeStreams() {
+    nameErrorController.close();
+    emailErrorController.close();
+    passwordErrorController.close();
+    passwordConfirmationErrorController.close();
+  }
+
   Future<void> loadPage(WidgetTester tester) async {
     final signUpPage = GetMaterialApp(
       initialRoute: '/signup',
@@ -14,6 +46,10 @@ void main() {
     );
     await tester.pumpWidget(signUpPage);
   }
+
+  tearDown(() {
+    closeStreams();
+  });
 
   testWidgets('Should load with correct initial state', (WidgetTester tester) async {
     await loadPage(tester);
@@ -49,5 +85,17 @@ void main() {
     final button = tester.widget<RaisedButton>(find.byType(RaisedButton));
     expect(button.onPressed, null);
     expect(find.byType(CircularProgressIndicator), findsNothing);
+  });
+
+  testWidgets('Should call validate with correct values', (WidgetTester tester) async {
+    await loadPage(tester);
+
+    final email = faker.internet.email();
+    await tester.enterText(find.bySemanticsLabel('Email'), email);
+    verify(presenter.validateEmail(email));
+
+    final password = faker.internet.password();
+    await tester.enterText(find.bySemanticsLabel('Senha'), password);
+    verify(presenter.validatePassword(password));
   });
 }
