4039294575acd42f078dea5f154f9973bca36451
IsraelLohuan <srlohuan@gmail.com>
2022-07-16 11:40:36 -0300

    ensure SurveysPage handles loading correctly

--- lib/ui/pages/surveys/surveys_page.dart
+++ lib/ui/pages/surveys/surveys_page.dart
@@ -1,3 +1,4 @@
+import 'package:ForDev/ui/components/spinner_dialog.dart';
 import 'package:ForDev/ui/helpers/helpers.dart';
 import 'package:ForDev/ui/pages/surveys/components/components.dart';
 import 'package:ForDev/ui/pages/surveys/surveys.dart';
@@ -18,20 +19,33 @@ class SurveysPage extends StatelessWidget {
         title: Text(R.strings.surveys),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 20),
-        child: CarouselSlider(
-          options: CarouselOptions(
-            enlargeCenterPage: true,
-            aspectRatio: 1
-          ),
-          items: [
-            SurveyItem(), 
-            SurveyItem(),
-            SurveyItem()
-          ],
-        ),
-      ),
+      body: Builder( 
+        builder: (context) {
+
+          presenter.isLoadingStream.listen((isLoading) {
+            if(isLoading == true) {
+              showLoading(context);
+            } else {
+              hideLoading(context);
+            }
+          });
+
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20),
+            child: CarouselSlider(
+              options: CarouselOptions(
+                enlargeCenterPage: true,
+                aspectRatio: 1
+              ),
+              items: [
+                SurveyItem(), 
+                SurveyItem(),
+                SurveyItem()
+              ],
+            ),
+          );
+        },
+      )
     );
   }
 }
--- test/ui/pages/surveys_page_test.dart
+++ test/ui/pages/surveys_page_test.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:ForDev/ui/pages/pages.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
 import 'package:mockito/mockito.dart';
@@ -6,8 +9,29 @@ import 'package:mockito/mockito.dart';
 class SurveysPresenterSpy extends Mock implements SurveysPresenter {}
 
 void main() {
-  testWidgets('Should call LoadSurveys on page load', (WidgetTester tester) async {
-    final presenter = SurveysPresenterSpy();
+  SurveysPresenterSpy presenter;
+  StreamController<bool> isLoadingController;
+
+  void initStreams() {
+    isLoadingController = StreamController();
+  }
+
+  void mockStreams() {
+    when(presenter.isLoadingStream).thenAnswer((_) => isLoadingController.stream);
+  }
+
+  void closeStreams() {
+    isLoadingController.close();
+  }
+
+  tearDown(() {
+    closeStreams();
+  });  
+
+  Future<void> loadPage(WidgetTester tester) async {
+    presenter = SurveysPresenterSpy();
+    initStreams();
+    mockStreams();
 
     final surveysPage = GetMaterialApp(
       initialRoute: '/surveys',
@@ -17,7 +41,31 @@ void main() {
     );
 
     await tester.pumpWidget(surveysPage);
+  }
+
+  testWidgets('Should call LoadSurveys on page load', (WidgetTester tester) async {
+    await loadPage(tester);
 
     verify(presenter.loadData()).called(1);
+  });
+
+  testWidgets('Should handle loading correctly', (WidgetTester tester) async {
+    await loadPage(tester);
+
+    isLoadingController.add(true);
+    await tester.pump();
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+
+    isLoadingController.add(false);
+    await tester.pump();
+    expect(find.byType(CircularProgressIndicator), findsNothing);
+
+    isLoadingController.add(true);
+    await tester.pump();
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+
+    isLoadingController.add(null);
+    await tester.pump();
+    expect(find.byType(CircularProgressIndicator), findsNothing);
   });
 }
