48084faac16b3b53486cbe117d3412ca9e2fdbfd
IsraelLohuan <srlohuan@gmail.com>
2022-07-18 13:31:50 -0300

    ensure LocalStorageAdapter return same value as cache on fetch

--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -12,10 +12,11 @@ void main() {
   LocalStorageSpy localStorage;
   String key;
   dynamic value;
+  String result;
 
   void mockDeleteError() => when(localStorage.deleteItem(any)).thenThrow(Exception());
   void mockSaveError() => when(localStorage.setItem(any, any)).thenThrow(Exception());
-
+  
   setUp(() {
     localStorage = LocalStorageSpy();
     sut = LocalStorageAdapter(localStorage: localStorage);
@@ -65,10 +66,22 @@ void main() {
   });
 
   group('fetch', () {
+    void mockFetch() => when(localStorage.getItem(any)).thenAnswer((_) async => result);
+
+    setUp(() {
+      mockFetch();
+    });
+
     test('Should call localStorage with correct values', () async {
       await sut.fetch(key);
 
       verify(localStorage.getItem(key)).called(1);
+    });
+
+    test('Should return same value as localStorage', () async {
+      final data = await sut.fetch(key);
+
+      expect(data, result);
     });
   });
 }
