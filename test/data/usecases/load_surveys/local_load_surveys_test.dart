82ab71e9c2e38156f12a468be23f59ad7921b079
IsraelLohuan <srlohuan@gmail.com>
2022-07-17 23:09:12 -0300

    ensure LocalLoadSurveys delete cache if its incomplete

--- test/data/usecases/load_surveys/local_load_surveys_test.dart
+++ test/data/usecases/load_surveys/local_load_surveys_test.dart
@@ -176,5 +176,16 @@ void main() {
 
       verify(cacheStorage.delete('surveys')).called(1);
     });
+
+    test('Should delete cache if it is incomplete', () async {
+      mockFetch([{
+        'date': '2020-07-20T00:00:00Z',
+        'didAnswer': 'false'
+      }]);
+
+      await sut.validate();
+
+      verify(cacheStorage.delete('surveys')).called(1);
+    });
   });
 }
