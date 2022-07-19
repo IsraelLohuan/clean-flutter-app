b479df76d0191f97a2accb3dd80d6379c1382eb7
IsraelLohuan <srlohuan@gmail.com>
2022-07-19 19:39:11 -0300

    add SurveyResult Entity

---
+++ lib/domain/entities/survey_answer_entity.dart
@@ -0,0 +1,16 @@
+
+import 'package:meta/meta.dart';
+
+class SurveyAnswerEntity {
+  final String image;
+  final String answer;
+  final bool isCurrentAnswer;
+  final int percent;
+
+  SurveyAnswerEntity({
+    this.image,
+    @required this.answer,
+    @required this.isCurrentAnswer,
+    @required this.percent
+  });
+}
