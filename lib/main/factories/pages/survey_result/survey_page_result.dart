4f8d01b1c22875eb3b749aca80518759d828559b
IsraelLohuan <srlohuan@gmail.com>
2022-07-23 10:26:28 -0300

    ensure SurveyResultPage calls LoadSurveyResult on page load

--- lib/main/factories/pages/survey_result/survey_page_result.dart
+++ lib/main/factories/pages/survey_result/survey_page_result.dart
@@ -2,4 +2,4 @@
 import 'package:ForDev/ui/pages/survey_result/survey_result_page.dart';
 import 'package:flutter/material.dart';
 
-Widget makeSurveyResultPage() => SurveyResultPage();
+Widget makeSurveyResultPage() => SurveyResultPage(null);
--- lib/ui/pages/survey_result/survey_result_page.dart
+++ lib/ui/pages/survey_result/survey_result_page.dart
@@ -1,8 +1,13 @@
 
 import 'package:ForDev/ui/helpers/helpers.dart';
+import 'package:ForDev/ui/pages/pages.dart';
 import 'package:flutter/material.dart';
 
 class SurveyResultPage extends StatelessWidget {
+  final SurveyResultPresenter presenter;
+
+  SurveyResultPage(this.presenter);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,64 +15,70 @@ class SurveyResultPage extends StatelessWidget {
         title: Text(R.strings.surveys),
         centerTitle: true,
       ),
-      body: ListView.builder(
-        itemCount: 3,
-        itemBuilder: (context, index) {
-          if(index == 0) {
-            return Container(
-              padding: EdgeInsets.only(top: 40, bottom: 20, left: 20, right: 20),
-              decoration: BoxDecoration(
-                color: Theme.of(context).disabledColor.withAlpha(90)
-              ),
-              child: Text('Qual é o seu framework web favorito?')
-            );
-          }
+      body: Builder(
+        builder: (context) {
+          presenter.loadData();
 
-          return Column(
-            children: [
-              Container(
-                padding: EdgeInsets.all(15),
-                decoration: BoxDecoration(
-                  color: Theme.of(context).backgroundColor
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Image.network(
-                      'http://fordevs.herokuapp.com/static/img/logo-angular.png',
-                      width: 40,
+          return ListView.builder(
+            itemCount: 3,
+            itemBuilder: (context, index) {
+              if(index == 0) {
+                return Container(
+                  padding: EdgeInsets.only(top: 40, bottom: 20, left: 20, right: 20),
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).disabledColor.withAlpha(90)
+                  ),
+                  child: Text('Qual é o seu framework web favorito?')
+                );
+              }
+
+              return Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).backgroundColor
                     ),
-                    Expanded(
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 10),
-                        child: Text(
-                          'Angular',
-                          style: TextStyle(fontSize: 16),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Image.network(
+                          'http://fordevs.herokuapp.com/static/img/logo-angular.png',
+                          width: 40,
                         ),
-                      ),
+                        Expanded(
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 10),
+                            child: Text(
+                              'Angular',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ),
+                        ),
+                        Text(
+                          '100%',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Theme.of(context).primaryColorDark
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 10),
+                          child: Icon(
+                            Icons.check_circle,
+                            color: Theme.of(context).highlightColor,
+                          ),
+                        )
+                      ],
                     ),
-                    Text(
-                      '100%',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                        color: Theme.of(context).primaryColorDark
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 10),
-                      child: Icon(
-                        Icons.check_circle,
-                        color: Theme.of(context).highlightColor,
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              Divider(height: 1,)
-            ],
+                  ),
+                  Divider(height: 1,)
+                ],
+              );
+            },
           );
-        },
+        } 
       ),
     );
   }
