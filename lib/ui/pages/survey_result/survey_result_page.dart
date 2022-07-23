266a0127511f74f4bd9a83ffd130871aa8d59b8b
IsraelLohuan <srlohuan@gmail.com>
2022-07-23 11:11:41 -0300

    move SurveyResult Component to its own file

--- lib/ui/pages/survey_result/survey_result_page.dart
+++ lib/ui/pages/survey_result/survey_result_page.dart
@@ -3,6 +3,7 @@ import 'package:ForDev/ui/components/components.dart';
 import 'package:ForDev/ui/helpers/helpers.dart';
 import 'package:ForDev/ui/pages/pages.dart';
 import 'package:flutter/material.dart';
+import 'components/components.dart';
 
 class SurveyResultPage extends StatelessWidget {
   final SurveyResultPresenter presenter;
@@ -36,65 +37,7 @@ class SurveyResultPage extends StatelessWidget {
               }
 
               if(snapshot.hasData) {
-                return ListView.builder(
-                  itemCount: 3,
-                  itemBuilder: (context, index) {
-                    if(index == 0) {
-                      return Container(
-                        padding: EdgeInsets.only(top: 40, bottom: 20, left: 20, right: 20),
-                        decoration: BoxDecoration(
-                          color: Theme.of(context).disabledColor.withAlpha(90)
-                        ),
-                        child: Text('Qual é o seu framework web favorito?')
-                      );
-                    }
-
-                    return Column(
-                      children: [
-                        Container(
-                          padding: EdgeInsets.all(15),
-                          decoration: BoxDecoration(
-                            color: Theme.of(context).backgroundColor
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Image.network(
-                                'http://fordevs.herokuapp.com/static/img/logo-angular.png',
-                                width: 40,
-                              ),
-                              Expanded(
-                                child: Padding(
-                                  padding: EdgeInsets.symmetric(horizontal: 10),
-                                  child: Text(
-                                    'Angular',
-                                    style: TextStyle(fontSize: 16),
-                                  ),
-                                ),
-                              ),
-                              Text(
-                                '100%',
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                  color: Theme.of(context).primaryColorDark
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(left: 10),
-                                child: Icon(
-                                  Icons.check_circle,
-                                  color: Theme.of(context).highlightColor,
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                        Divider(height: 1,)
-                      ],
-                    );
-                  },
-                );
+                return SurveyResult();
               }
 
               return SizedBox(height: 0,);
@@ -105,4 +48,6 @@ class SurveyResultPage extends StatelessWidget {
     );
   }
 }
+
+
 
