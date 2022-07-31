836913e4e9f764c4aab78183253ff331fb3c7218
IsraelLohuan <srlohuan@gmail.com>
2022-07-30 22:14:04 -0300

    allows navigation stream to send the same event trace

--- test/presentation/presenter/getx_surveys_presenter_test.dart
+++ test/presentation/presenter/getx_surveys_presenter_test.dart
@@ -74,8 +74,14 @@ void main() {
   });
 
   test('Should go to SurveyResultPage on survey click', () async {
+    expectLater(sut.navigateToStream, emitsInOrder([
+      '/survey_result/1',
+      '/survey_result/1'
+    ]));
+
     sut.navigateToStream.listen(expectAsync1((page) => expect(page, '/survey_result/1')));
 
+    sut.goToSurveyResult('1');
     sut.goToSurveyResult('1');
   });
 }
